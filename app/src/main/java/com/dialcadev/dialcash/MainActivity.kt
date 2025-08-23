608c5775adc7c1b56774147d073afbbe32ce6752
Diego17 <diegoalbertocastropastor@gmail.com>
2025-08-23 13:40:54 -0500

    Implement user registration check in MainActivity and navigate to RegisterActivity if not registered
    Signed-off-by: Diego17 <diegoalbertocastropastor@gmail.com>

--- app/src/main/java/com/dialcadev/dialcash/MainActivity.kt
+++ app/src/main/java/com/dialcadev/dialcash/MainActivity.kt
@@ -1,34 +1,51 @@
 package com.dialcadev.dialcash
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.dialcadev.dialcash.data.UserDataStore
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var userDataStore: UserDataStore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
-        findViewById<android.view.View>(android.R.id.content).post {
-            val navController = findNavController(R.id.nav_host_fragment)
-            val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
-            bottomNav.setupWithNavController(navController)
-            navController.addOnDestinationChangedListener { _, destination, _ ->
-                supportActionBar?.title = destination.label
+        userDataStore = UserDataStore.getInstance(this)
+        lifecycleScope.launch {
+            val isRegistered = userDataStore.isUserRegistered().first()
+            if (!isRegistered) {
+                val intent = Intent(this@MainActivity, RegisterActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+                finish()
+                return@launch
+            }
+            enableEdgeToEdge()
+            setContentView(R.layout.activity_main)
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
+            val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+            setSupportActionBar(toolbar)
+            findViewById<android.view.View>(android.R.id.content).post {
+                val navController = findNavController(R.id.nav_host_fragment)
+                val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+                bottomNav.setupWithNavController(navController)
+                navController.addOnDestinationChangedListener { _, destination, _ ->
+                    supportActionBar?.title = destination.label
+                }
             }
         }
     }
