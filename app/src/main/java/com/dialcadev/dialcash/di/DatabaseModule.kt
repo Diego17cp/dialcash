e60150d0fa805aa5a5ca2754674fdfa569b0f297
Diego17 <diegoalbertocastropastor@gmail.com>
2025-08-22 22:40:35 -0500

    Add DatabaseModule for Hilt dependency injection of AppDB and AppRepository
    Signed-off-by: Diego17 <diegoalbertocastropastor@gmail.com>

---
+++ app/src/main/java/com/dialcadev/dialcash/di/DatabaseModule.kt
@@ -0,0 +1,27 @@
+package com.dialcadev.dialcash.di
+
+import android.content.Context
+import com.dialcadev.dialcash.data.AppRepository
+import com.dialcadev.dialcash.data.db.AppDB
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule{
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): AppDB {
+        return AppDB.getInstance(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAppRepository(db: AppDB): AppRepository {
+        return AppRepository(db)
+    }
+}
