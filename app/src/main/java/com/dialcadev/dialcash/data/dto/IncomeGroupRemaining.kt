cbb031bcac9af82d63ff2a1ac61b7d075f0a098c
Diego17 <diegoalbertocastropastor@gmail.com>
2025-08-22 21:21:35 -0500

    Refactor IncomeGroupRemaining data class to clarify amount and remaining fields
    Signed-off-by: Diego17 <diegoalbertocastropastor@gmail.com>

--- app/src/main/java/com/dialcadev/dialcash/data/dto/IncomeGroupRemaining.kt
+++ app/src/main/java/com/dialcadev/dialcash/data/dto/IncomeGroupRemaining.kt
@@ -3,5 +3,6 @@ package com.dialcadev.dialcash.data.dto
 data class IncomeGroupRemaining (
     val id: Int,
     val name: String,
-    val restante: Double, // Remaining amount in the income group
+    val amount: Double, // Total amount allocated to the income group
+    val remaining: Double, // Remaining amount in the income group
 )
