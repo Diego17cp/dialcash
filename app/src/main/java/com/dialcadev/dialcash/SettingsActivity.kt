80d62ebb126462936f338177cbb329d4b807517d
Diego17 <diegoalbertocastropastor@gmail.com>
2025-09-02 22:19:25 -0500

    Refactor SettingsActivity to use ViewBinding and enhance UI with edge-to-edge support
    Signed-off-by: Diego17 <diegoalbertocastropastor@gmail.com>

--- app/src/main/java/com/dialcadev/dialcash/SettingsActivity.kt
+++ app/src/main/java/com/dialcadev/dialcash/SettingsActivity.kt
@@ -1,14 +1,59 @@
 package com.dialcadev.dialcash
 
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.dialcadev.dialcash.data.UserDataStore
+import com.dialcadev.dialcash.databinding.SettingsActivityBinding
+import kotlinx.coroutines.launch
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: SettingsActivityBinding
+    private lateinit var userDataStore: UserDataStore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.settings_activity)
+        binding = SettingsActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.settings)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        userDataStore = UserDataStore.getInstance(this)
+        setupViews()
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressedDispatcher.onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setupViews() {
+        lifecycleScope.launch {
+            userDataStore.getUserData().collect { user ->
+                binding.tvUsername.text = user.name
+                val uri = user.photoUri
+                    .takeIf { it.isNotBlank() }
+                    ?.let { Uri.parse(it) }
+                binding.imageProfile.setImageURI(uri)
+            }
+        }
     }
 }
