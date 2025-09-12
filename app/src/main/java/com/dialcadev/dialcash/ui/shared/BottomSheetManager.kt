fa68e17f0621af2a28b1fa220512326e2b72c703
Diego17 <diegoalbertocastropastor@gmail.com>
2025-09-11 22:23:38 -0500

    Add BottomSheetManager to display account and transaction bottom sheets
    Signed-off-by: Diego17 <diegoalbertocastropastor@gmail.com>

---
+++ app/src/main/java/com/dialcadev/dialcash/ui/shared/BottomSheetManager.kt
@@ -0,0 +1,53 @@
+package com.dialcadev.dialcash.ui.shared
+
+import android.content.Context
+import android.view.LayoutInflater
+import com.dialcadev.dialcash.data.dao.AccountBalanceWithOriginal
+import com.dialcadev.dialcash.data.dto.TransactionWithDetails
+import com.dialcadev.dialcash.data.entities.Account
+import com.dialcadev.dialcash.data.entities.IncomeGroup
+import com.dialcadev.dialcash.data.entities.Transaction
+import com.dialcadev.dialcash.databinding.RecycleAccountItemBinding
+import com.dialcadev.dialcash.databinding.RecycleTransactionItemBinding
+import com.dialcadev.dialcash.ui.shared.handlers.AccountBottomSheetHandler
+import com.dialcadev.dialcash.ui.shared.handlers.TransactionBottomSheetHandler
+import com.google.android.material.bottomsheet.BottomSheetDialog
+
+class BottomSheetManager(private val context: Context, private val layoutInflater: LayoutInflater) {
+    fun showAccountBottomSheet(
+        account: AccountBalanceWithOriginal,
+        onUpdate: (Account) -> Unit,
+        onDelete: (Account) -> Unit
+    ) {
+        val dialog = BottomSheetDialog(context)
+        val binding = RecycleAccountItemBinding.inflate(layoutInflater)
+
+        AccountBottomSheetHandler(binding, account, onUpdate, onDelete, dialog).setup()
+        dialog.setContentView(binding.root)
+        dialog.show()
+    }
+
+    fun showTransactionBottomSheet(
+        transaction: TransactionWithDetails,
+        accounts: List<Account>,
+        incomeGroups: List<IncomeGroup>,
+        onUpdate: (Transaction) -> Unit,
+        onDelete: (Transaction) -> Unit,
+        onValidateBalance: (Int, String, Int, Double, Int?, Int?, (Boolean, String?) -> Unit) -> Unit
+    ) {
+        val dialog = BottomSheetDialog(context)
+        val binding = RecycleTransactionItemBinding.inflate(layoutInflater)
+        TransactionBottomSheetHandler(
+            binding,
+            transaction,
+            accounts,
+            incomeGroups,
+            onUpdate,
+            onDelete,
+            onValidateBalance,
+            dialog
+        ).setup()
+        dialog.setContentView(binding.root)
+        dialog.show()
+    }
+}
