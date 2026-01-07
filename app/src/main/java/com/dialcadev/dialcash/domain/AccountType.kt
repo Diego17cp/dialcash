895143085f1589034efd3d1f6ce5106f79556e21
Diego17 <diegoalbertocastropastor@gmail.com>
2026-01-07 13:51:20 -0500

    Add accountTypes list for UI representation of account types
    Signed-off-by: Diego17 <diegoalbertocastropastor@gmail.com>

--- app/src/main/java/com/dialcadev/dialcash/domain/AccountType.kt
+++ app/src/main/java/com/dialcadev/dialcash/domain/AccountType.kt
@@ -1,6 +1,7 @@
 package com.dialcadev.dialcash.domain
 
 import com.dialcadev.dialcash.R
+import com.dialcadev.dialcash.data.dto.AccountTypeUI
 
 enum class AccountType(val code: String, val labelRes: Int) {
     BANK("bank", R.string.account_type_bank_account),
@@ -15,3 +16,13 @@ enum class AccountType(val code: String, val labelRes: Int) {
         fun byCode(code: String) = AccountType.entries.firstOrNull { it.code == code } ?: BANK
     }
 }
+
+val accountTypes = listOf<AccountTypeUI>(
+    AccountTypeUI("bank", R.string.account_type_bank_account, R.drawable.ic_building_bank),
+    AccountTypeUI("card", R.string.account_type_credit_card, R.drawable.ic_card),
+    AccountTypeUI("cash", R.string.account_type_cash, R.drawable.ic_cash),
+    AccountTypeUI("wallet", R.string.account_type_wallet, R.drawable.ic_accounts_filled),
+    AccountTypeUI("savings", R.string.savings, R.drawable.ic_bank),
+    AccountTypeUI("debt", R.string.account_type_debt, R.drawable.ic_debt_payment),
+    AccountTypeUI("other", R.string.account_type_other, R.drawable.ic_account_default)
+)
