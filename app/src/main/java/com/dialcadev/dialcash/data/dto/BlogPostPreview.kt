4f5c6bf49c29c0226f22bc11d9049693887c9b42
Diego17 <diegoalbertocastropastor@gmail.com>
2026-01-08 12:02:16 -0500

    Add BlogPostPreview data class for blog post representation
    Signed-off-by: Diego17 <diegoalbertocastropastor@gmail.com>

---
+++ app/src/main/java/com/dialcadev/dialcash/data/dto/BlogPostPreview.kt
@@ -0,0 +1,10 @@
+package com.dialcadev.dialcash.data.dto
+
+data class BlogPostPreview(
+    val title: String,
+    val description: String,
+    val portrait: String,
+    val publishedAt: String,
+    val category: String,
+    val slug: String
+)
