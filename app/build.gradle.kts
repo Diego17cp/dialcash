ececb237d3ca6b586be3a2c86fd75792a2f26d6a
Diego17 <diegoalbertocastropastor@gmail.com>
2026-01-09 12:28:11 -0500

    Update version name to 1.1 and enable buildConfig feature
    Signed-off-by: Diego17 <diegoalbertocastropastor@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -35,6 +35,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
     kotlinOptions {
         jvmTarget = "11"
