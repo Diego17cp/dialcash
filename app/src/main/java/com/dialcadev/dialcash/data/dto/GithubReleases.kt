d662fdcaa087c609692a97014eb877de4fd0a03a
Diego17 <diegoalbertocastropastor@gmail.com>
2026-01-09 12:29:26 -0500

    Add data classes for GitHub releases and assets
    Signed-off-by: Diego17 <diegoalbertocastropastor@gmail.com>

---
+++ app/src/main/java/com/dialcadev/dialcash/data/dto/GithubReleases.kt
@@ -0,0 +1,13 @@
+package com.dialcadev.dialcash.data.dto
+
+data class GithubReleaseDto(
+    val tag_name: String,
+    val body: String,
+    val assets: List<GithubAssetDto>
+)
+
+data class GithubAssetDto(
+    val name: String,
+    val browser_download_url: String
+)
+
